42e519ed28b3e428c0473a88bf806ac912373c41
azlan.hussein <vhussein80@gmail.com>
2017-04-27 13:51:18 +0800

    Commit with initial hello world

---
+++ helloWorld/src/main/java/org/azlan/helloWorld/testHelloWorld.java
@@ -0,0 +1,12 @@
+package org.azlan.helloWorld;
+
+/**
+ * Created by MohdAzlan.Hussein on 4/27/2017.
+ */
+public class testHelloWorld {
+
+    public static void main(String [] args)
+    {
+        System.out.println("This is the initial hello world app");
+    }
+}
